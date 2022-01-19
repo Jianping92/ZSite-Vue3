f4c62b843193ff068c1407a7f3fd663226575735
赵建平 <zhao_jianping@cdv.com>
2022-01-19 15:22:04 +0800

    feat-环境变量、配置项等;

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,25 @@
 // const path = require("path");
+// const fs = require("fs");
+// const resolve = (dir) => path.join(__dirname, dir);
+const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV);
+// const IS_DEV = ["development"].includes(process.env.NODE_ENV);
 
-module.exports = {};
+module.exports = {
+  publicPath: IS_PROD ? process.env.VUE_APP_PUBLIC_PATH : "./", // 默认'/'，部署应用包时的基本 URL
+  outputDir: "dist",
+  assetsDir: "static",
+  // 去除生产环境的 source map
+  productionSourceMap: !IS_PROD,
+  devServer: {
+    open: true,
+    hot: true,
+    port: 7878,
+    proxy: {
+      "/api": {
+        target: "https://www.baidu.com",
+        secure: false,
+        changeOrigin: true
+      }
+    }
+  }
+};
